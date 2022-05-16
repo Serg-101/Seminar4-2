1ed2923570931e0af3f319e9b1edf19b83c8e075
Serg-101 <golikov1979@gmail.com>
2022-05-16 23:33:24 +0300

    first commit

---
+++ Dz-19/Program.cs
@@ -0,0 +1,57 @@
+﻿/* Задача 19: Напишите программу,
+ которая принимает на вход пятизначное число
+  и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+23432 -> да
+12821 -> да
+ */
+
+
+bool GetPolindrom(int num)
+{
+    string numReverse = new string(num.ToString().Reverse().ToArray());
+    string numFirst = num.ToString().Substring(0,2); 
+    string numLast = numReverse.Substring(0,2);
+    if (numFirst == numLast)
+        return true;
+    else
+        return false;
+}
+
+
+
+
+//функция, которая получает число, введенное с клавиатуры
+// + проверка на то, что ввели именно число
+// + проверка, что число из 5 знаков
+int GetNumber(string msg)
+{
+    while (true)
+    {
+        Console.Write(msg);
+        string valueFromConsole = Console.ReadLine();
+
+        if (int.TryParse(valueFromConsole, out int number))
+        {
+            if (number.ToString().Length == 5)
+                return number;
+            else
+                Console.WriteLine("Вы ввели не пятизначное число. Попробуйте ввести пятизначное число.");
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число. Нужно ввести число, отличное от нуля.");
+        }
+    }
+}
+
+string messageI = "Введите пятизначное число А = ";
+int a = GetNumber(messageI);
+
+bool isPolindrom = GetPolindrom(a);
+
+if (isPolindrom)
+    Console.WriteLine("Число является полиндромом");
+else
+    Console.WriteLine("Число не является полиндромом");
---
+++ Dz-20/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Задача 21: Напишите программу,
+ которая принимает на вход координаты двух точек
+ и находит расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+
+ */
+
+
+double GetLength(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double leng = Math.Sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2))+((z1-z2)*(z1-z2)));
+    leng = Math.Round(leng, 2);
+    return leng;
+}
+
+
+//функция, которая получает число, введенное с клавиатуры
+// + проверка на то, что ввели именно число
+int GetNumber(string msg)
+{
+    while (true)
+    {
+        Console.WriteLine(msg);
+        string valueFromConsole = Console.ReadLine();
+
+        if (int.TryParse(valueFromConsole, out int number))
+        {
+            return number;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число. Нужно ввести число, отличное от нуля.");
+        }
+    }
+}
+
+System.Console.WriteLine("Для расчета растояния введите координаты точек А и В");
+string messageI = "Введите Координату точки А!  Х=";
+int x1 = GetNumber(messageI);
+messageI = "Введите Координату точки А!  Y=";
+int y1 = GetNumber(messageI);
+messageI = "Введите Координату точки А!  Z=";
+int z1 = GetNumber(messageI);
+messageI = "Введите Координату точки B!  Х=";
+int x2 = GetNumber(messageI);
+messageI = "Введите Координату точки B!  Y=";
+int y2 = GetNumber(messageI);
+messageI = "Введите Координату точки B!  Z=";
+int z2 = GetNumber(messageI);
+
+double leng = GetLength(x1, y1, z1, x2, y2, z2);
+
+
+Console.WriteLine($"Расстояние между А и B  = {leng}");
